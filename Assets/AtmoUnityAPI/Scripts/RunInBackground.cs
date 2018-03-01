5a5ec3ec5dda63a4c2c8a15f9cccd8f4df25a60f
Balint <zjqgv5@gmail.com>
2018-03-01 11:44:29 +0100

    Add RunInBackground

---
+++ Assets/AtmoUnityAPI/Scripts/RunInBackground.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RunInBackground : MonoBehaviour {
+
+    private void OnEnable()
+    {
+        Application.runInBackground = true;
+    }
+
+    private void OnDisable()
+    {
+        Application.runInBackground = false;
+    }
+}
