a6805cdfa289a7c56cea7fc1e37a4662ac46c305
Balint <zjqgv5@gmail.com>
2018-03-08 20:01:49 +0200

    New emulator works with all 3 events.
    Todo:
    1) Hidden animation
    2) Marker selector

---
+++ Assets/Emulator/Scripts/EmulatedMarker.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EmulatedMarker : MonoBehaviour {
+
+    public Marker marker;
+    public bool hidden = false;
+    public float hideTimeStamp = 0;
+
+    private Sprite diceFace;
+
+	// Use this for initialization
+	void Start () {
+        diceFace = GetComponent<Sprite>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void Hide() 
+    {
+        hidden = true;
+        hideTimeStamp = Time.time;
+    }
+
+    public void Unhide()
+    {
+        hidden = false;
+    }
+
+    public void Remove()
+    {
+        Destroy(gameObject);
+    }
+}
--- Assets/Emulator/Scripts/TrackingEmulator.cs
+++ Assets/Emulator/Scripts/TrackingEmulator.cs
@@ -6,46 +6,102 @@ public class TrackingEmulator : MonoBehaviour {
 
     // for debug - to generate marker event with mouse click
     private TrackingHandler trackingHandler;
-    private List<Marker> _markers = new List<Marker>();
+    private List<EmulatedMarker> existingMarkers = new List<EmulatedMarker>();
+    private List<EmulatedMarker> hiddenMarkers = new List<EmulatedMarker>();
+
+    public GameObject[] markerSprites = new GameObject[6];
 
     private int uniqueId = 0;
     private float markerRadius = 0.2395834f;
 
-    public int markerId;
+    public int _markerId;
 
 	// Use this for initialization
 	void Start () {
-        // for debug - to generate marker event with mouse click
         trackingHandler = GameObject.Find("AtmoTracking").GetComponent<TrackingHandler>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-        bool isLostEvent = false;
 
         if (Input.GetMouseButtonUp(0))
         {
             Vector2 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
 
-            // check collision with exsting marker
-            for (int i = _markers.Count - 1; i >= 0; i--)
-            {
-                // trigger onLost event if clicked in existing marker
-                if (Vector2.Distance(_markers[i].position, mousePosition) < markerRadius)
-                {
-                    trackingHandler.onLostEvent.Invoke(_markers[i]);
-                    _markers.Remove(_markers[i]);
-                    isLostEvent = true;
-                }
-            }
+            EmulatedMarker existingMarker = CheckCollisionWithExistingMarker(mousePosition);
 
-            // if there's no existing marker on the mouse position trigger onDetected event
-            if (!isLostEvent)
+            if (existingMarker == null)
+                AddNewMarker(mousePosition, _markerId);
+
+            else if (existingMarker.hidden)
+                UnhideMarker(existingMarker);
+
+            else
+                HideMarker(existingMarker);
+        }
+
+        RemoveOverdueHiddenMarkers();
+	}
+
+    private EmulatedMarker CheckCollisionWithExistingMarker(Vector2 mousePosition)
+    {
+        EmulatedMarker existingMarker = null;
+
+        // check collision with exsting marker
+        for (int i = existingMarkers.Count - 1; i >= 0; i--)
+        {
+            // trigger onLost event if clicked in existing marker
+            if (Vector2.Distance(existingMarkers[i].marker.position, mousePosition) < markerRadius)
             {
-                Marker newMarker = new Marker(0, markerId, uniqueId++, mousePosition);
-                trackingHandler.onDetectedEvent.Invoke(newMarker);
-                _markers.Add(newMarker);
+                existingMarker = existingMarkers[i];
             }
         }
-	}
+
+        return existingMarker;
+    }
+
+    private void AddNewMarker(Vector3 mousePosition, int markerId)
+    {
+        EmulatedMarker newMarker = Instantiate(markerSprites[markerId], mousePosition, Quaternion.identity).GetComponent<EmulatedMarker>();
+        newMarker.marker = new Marker(0, _markerId, uniqueId++, mousePosition);
+        existingMarkers.Add(newMarker);
+        trackingHandler.onDetectedEvent.Invoke(newMarker.marker);
+    }
+
+    private void RemoveMarker(EmulatedMarker markerToRemove)
+    {
+        existingMarkers.Remove(markerToRemove);
+        markerToRemove.Remove();
+    }
+
+    private void HideMarker(EmulatedMarker marker)
+    {
+        trackingHandler.onLostEvent.Invoke(marker.marker);
+        marker.Hide();
+        hiddenMarkers.Add(marker);
+    }
+
+    private void UnhideMarker(EmulatedMarker hiddenMarker)
+    {
+        hiddenMarker.Unhide();
+        hiddenMarkers.Remove(hiddenMarker);
+        trackingHandler.onRedetectedEvent.Invoke(hiddenMarker.marker);
+    }
+
+    private void RemoveOverdueHiddenMarkers()
+    {
+        if (hiddenMarkers.Count > 0)
+        {
+            for (int i = hiddenMarkers.Count - 1; i >= 0;i--)
+            {
+                if (Time.time - hiddenMarkers[i].hideTimeStamp >= 5)
+                {
+                    RemoveMarker(hiddenMarkers[i]);
+                    hiddenMarkers.Remove(hiddenMarkers[i]);
+                }
+            }
+        }
+    }
+
+
 }
