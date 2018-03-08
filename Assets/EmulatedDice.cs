16faf3d9718228565d46279ba0c64ddcdebb2c37
Balint <zjqgv5@gmail.com>
2018-03-08 18:52:14 +0200

    Add EmulatedDice

---
+++ Assets/EmulatedDice.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EmulatedDice : MonoBehaviour {
+
+    private int markerId;
+
+	// Use this for initialization
+	void Start () {
+        markerId = int.Parse(gameObject.name);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
