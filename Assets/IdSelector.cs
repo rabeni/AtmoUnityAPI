8ba710d89105a148aa9864bc28c846e51aa4acee
foltri <zjqgv5@gmail.com>
2018-03-31 15:40:26 +0200

    New dice event when IdSelector open bug solved

--- Assets/IdSelector.cs
+++ Assets/IdSelector.cs
@@ -21,11 +21,16 @@ public class IdSelector : MonoBehaviour {
 
         trackingEmulator = transform.parent.GetComponent<TrackingEmulator>();
 
+		// Disable boxcollider while selector is closed
+		gameObject.GetComponent<BoxCollider2D> ().enabled = false;
+
+		// Get IdSelectorMarkers
         for (int i = 0; i < transform.childCount; i++)
         {
             markers[i] = transform.GetChild(i).GetComponent<IdSelectorMarker>();
         }
 
+		// Select default chosen id
         markers[currentId].Select();
 
 	}
@@ -48,7 +53,8 @@ public class IdSelector : MonoBehaviour {
         if (state == 1)
         {
             Vector3 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-            mousePosition.z = selectorZ;
+			// with different z, there's no match
+			mousePosition.z = selectorMarkerZ;
             int id = CheckCollisionWithMarkerSelector(mousePosition);
 
             // if mouse is colliding with an IdSelectorMarker
@@ -63,36 +69,44 @@ public class IdSelector : MonoBehaviour {
         }
     }
 
+	// if selector is open and there's a click, close it
+	void OnMouseUp() 
+	{
+		Close ();
+	}
+
     private void Open(Vector3 center)
     {
         int numOfItems = 6;
         float radius = 0.6f;
 
-        transform.position = center;
+//        transform.position = center;
 
         for (int i = 0; i < numOfItems; i++)
         {
             float angle = i * Mathf.PI * 2 / numOfItems;
-            Vector3 pos = new Vector3(Mathf.Cos(angle), Mathf.Sin(angle), 0) * radius + center;
+			Vector3 pos = new Vector3(Mathf.Cos(angle), Mathf.Sin(angle), 0) * radius + center;
 
-            //markers[i].position = pos;
-            StartCoroutine(Translate_c(markers[i].transform, pos));
+			StartCoroutine(Translate_c(markers[i].transform, center, pos));
         }
+
+		// enable selector layer collider
+		gameObject.GetComponent<BoxCollider2D> ().enabled = true;
     }
 
-    private IEnumerator Translate_c(Transform targetTransform, Vector3 end)
+	private IEnumerator Translate_c(Transform targetTransform, Vector3 start, Vector3 end)
     {
         float duration = 0.2f;
         float resolution = duration * 30f; //30 FPS
 
         float step = duration / resolution;
-        Vector3 start = targetTransform.position;
+//        Vector3 start = targetTransform.position;
 
         for (int i = 1; i < resolution + 1; i++)
         {
             float x = EasingEquations.EaseOutCubic(start.x, end.x, i / resolution);
             float y = EasingEquations.EaseOutCubic(start.y, end.y, i / resolution);
-            targetTransform.position = new Vector3(x, y, targetTransform.position.z);
+			targetTransform.position = new Vector3 (x, y, selectorMarkerZ);//targetTransform.position.z);
 
             float scale = EasingEquations.EaseOutCubic(0, 1, i / resolution);
             targetTransform.localScale = new Vector3(scale, scale, scale);
@@ -105,17 +119,19 @@ public class IdSelector : MonoBehaviour {
     {
         for (int i = 0; i < 6; i++)
         {
-            markers[i].transform.localPosition = new Vector3(0, 0, 0);
+			markers[i].transform.localPosition = new Vector3(0, 0, selectorMarkerZ);
 
             markers[i].transform.localScale = new Vector3(0, 0, 0);
         }
+
+		// disable selector layer collider
+		gameObject.GetComponent<BoxCollider2D> ().enabled = false;
     }
 
     private int CheckCollisionWithMarkerSelector(Vector3 mousePosition)
     {
         int index = -1;
 
-        // check collision with exsting marker
         for (int i = 0; i < 6; i++)
         {
             if (markers[i].transform.GetComponent<Collider2D>().bounds.Contains(mousePosition))
