5902a60ca84bffd09a59daf8000da1e850c2c2e8
Bakó A. Benjamin <bako.benjamin@autsoft.hu>
2019-10-05 19:24:56 +0200

    spacing

--- Assets/Scripts/MapGenerator.cs
+++ Assets/Scripts/MapGenerator.cs
@@ -6,26 +6,34 @@ public class MapGenerator : MonoBehaviour
 {
     [SerializeField] private GameObject _hexPrefab;
     [SerializeField] private float _depth;
-
+    [SerializeField] private Vector2 _spacing=Vector2.one;
+    private float hexHeight; 
+    private float hexWidth;
+    private Vector3 lowerLeftScreen;
     private Transform _mapParent;
     // Start is called before the first frame update
     void Start()
     {
+        Init();
         GenerateMap();
+    }
+
+    private void Init()
+    {
+        hexHeight = _hexPrefab.GetComponent<SpriteRenderer>().size.y * _hexPrefab.transform.lossyScale.y* _spacing.y;
+        hexWidth = _hexPrefab.GetComponent<SpriteRenderer>().size.x * _hexPrefab.transform.lossyScale.x*_spacing.x;
+        lowerLeftScreen = Camera.main.ViewportToWorldPoint(new Vector3(0, 0, -Camera.main.transform.position.z));
+
     }
 
     private void GenerateMap()
     {
         _mapParent = new GameObject("MapParent").transform;
 
-        
-
         int horizontalIterCnt, verticalIterCnt;
         GetIterationCount(out horizontalIterCnt, out verticalIterCnt);
-
-        var lowerLeftScreen = Camera.main.ViewportToWorldPoint(new Vector3(0, 0, -Camera.main.transform.position.z));
-        var hexHeight = _hexPrefab.GetComponent<SpriteRenderer>().size.y * _hexPrefab.transform.lossyScale.y;
-        var hexWidth = _hexPrefab.GetComponent<SpriteRenderer>().size.x * _hexPrefab.transform.lossyScale.x;
+        
+        
         var col = new GameObject("Col").transform;
         col.SetParent(_mapParent);
         col.position = lowerLeftScreen + verticalIterCnt / 2 * Vector3.up;
@@ -38,8 +46,8 @@ public class MapGenerator : MonoBehaviour
 
         Vector3 downDisplacement = new Vector3(hexWidth*0.75f, -hexHeight*0.5f, 0f);
         Vector3 upDisplacement = new Vector3(hexWidth*0.75f, hexHeight*0.5f, 0f);
-
         Vector3 lastRowPosition = col.transform.position;
+
         for (int x = 1; x < horizontalIterCnt; x++)
         {
             lastRowPosition = Instantiate(col, lastRowPosition + (x % 2 == 1 ? downDisplacement : upDisplacement), Quaternion.identity, _mapParent).transform.position;
@@ -48,11 +56,8 @@ public class MapGenerator : MonoBehaviour
 
     private void GetIterationCount(out int horizontalIterCnt, out int verticalIterCnt)
     {
-        var lowerLeftScreen = Camera.main.ViewportToWorldPoint(new Vector3(0, 0, -Camera.main.transform.position.z));
         var upperLeftScreen = Camera.main.ViewportToWorldPoint(new Vector3(0, 1, -Camera.main.transform.position.z));
         var lowerRightScreen = Camera.main.ViewportToWorldPoint(new Vector3(1, 0, -Camera.main.transform.position.z));
-        var hexHeight = _hexPrefab.GetComponent<SpriteRenderer>().size.y * _hexPrefab.transform.lossyScale.y;
-        var hexWidth = _hexPrefab.GetComponent<SpriteRenderer>().size.x * _hexPrefab.transform.lossyScale.x;
 
         horizontalIterCnt = (int)((Vector3.Distance(lowerLeftScreen, lowerRightScreen) / (hexWidth*1.5)))*2+4;
         verticalIterCnt = (int)(Vector3.Distance(lowerLeftScreen, upperLeftScreen) / (hexHeight)+2);
